e61d17816425847a111e62ed38541a29b2bb2bed
OferShapsa <32899760+ofershapsa@users.noreply.github.com>
2020-07-19 13:44:55 +0300

    Delete ContactsService.js

--- src/service/ContactsService.js
+++
@@ -1,8 +0,0 @@
-import contactsSample from './contacts-sample.json'
-import axios from 'axios'
-
-export default class ContactsService {
-    getContacts() {
-       return axios.get('localhost:8080/getAll')
-    }
-}
